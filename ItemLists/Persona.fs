aed84f70e9aeb593272b73815bbaf01a504c05b0
RKO <rkosafo@outlook.com>
2019-06-06 16:22:47 +0000

    Added Persona

--- ItemLists/Persona.fs
+++ ItemLists/Persona.fs
@@ -1,2 +1,33 @@
-﻿module Persona
+﻿namespace UiFabric
 
+
+open Fable.Core
+open Fable.React
+open Fable.React.Props
+open Utils
+
+[<RequireQualifiedAccess>]
+module Persona =
+  type IPersonaProps =
+    | Props of IHTMLProp list
+    | ClassName of string
+    | Text of string
+    | SecondaryText of string
+    | TertiaryText of string
+    | OptionalText of string
+    | Size of int
+    | CoinSize of int
+    | ImageUrl of string
+    | ImageInitials of string
+    | ShowUnknownPersonaCoin of bool
+    | Styles of obj
+    | Theme of ITheme
+    interface IHTMLProp
+    static member p props =
+      props
+      |> List.fold (fun s x -> match x with
+                               | Props x -> s @ x
+                               | x -> (x :> IHTMLProp) :: s) []
+      |> kvl
+
+  let persona props = ofImport "Persona" ImportPath (IPersonaProps.p props) []
