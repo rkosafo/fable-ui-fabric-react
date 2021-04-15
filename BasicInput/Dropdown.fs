49a2ac8f4e0513b4c0862656d70834137a9a91cd
Jordan Marr <jmarr@microdesk.com>
2021-04-15 12:21:02 -0400

    Added helpers for Dropdown

--- BasicInput/Dropdown.fs
+++ BasicInput/Dropdown.fs
@@ -1,6 +1,5 @@
 ﻿namespace Fable.UIFabric
 
-
 open Fable.Core
 open Fable.React
 open Fable.React.Props
@@ -17,38 +16,43 @@ type IDropdownOption =
 
 [<RequireQualifiedAccess>]
 module Dropdown =
-  type IDropdownProps =
-    | Props of IHTMLProp list
-    | DefaultSelectedKeys of int []
-    | DropdownWidth of int
-    | MultiSelect of bool
-    | MultiSelectDelimiter of string
-    | NotifyOnReselect of bool
-    | OnChange of (Browser.Types.Event -> IDropdownOption -> int -> unit)
-    | OnDismiss of (unit -> unit)
-    | Placeholder of string
-    | OnRenderPlaceholder of (IDropdownOption -> ReactElement)
-    | OnRenderTitle of (IDropdownOption -> ReactElement)
-    | Options of IDropdownOption []
-    | ResponsiveMode of ResponsiveMode
-    | SelectedKeys of int []
-    | [<CompiledName("selectedKeys")>]SelectedKeysStr of string []
-    | SelectedKey of int
-    | [<CompiledName("selectedKey")>]SelectedKeyStr of string
-    | ErrorMessage of string
-    | Styles of obj
-    | Theme of ITheme
-    | Label of string
-    | Disabled of bool
-    | Value of obj
-    | Required of bool
-    interface IHTMLProp
-    static member p props =
-      props
-      |> List.fold (fun s x -> match x with
-                               | Props x -> s @ x
-                               | x -> (x :> IHTMLProp) :: s) []
-      |> kvl
+    type IDropdownProps =
+        | Props of IHTMLProp list
+        | DefaultSelectedKeys of int []
+        | DefaultSelectedKey of string
+        | DropdownWidth of int
+        | MultiSelect of bool
+        | MultiSelectDelimiter of string
+        | NotifyOnReselect of bool
+        | OnChange of (Browser.Types.Event -> IDropdownOption -> int -> unit)
+        | OnDismiss of (unit -> unit)
+        | Placeholder of string
+        | OnRenderPlaceholder of (IDropdownOption -> ReactElement)
+        | OnRenderTitle of (IDropdownOption -> ReactElement)
+        | Options of IDropdownOption []
+        | ResponsiveMode of ResponsiveMode
+        | SelectedKeys of int []
+        | [<CompiledName("selectedKeys")>]SelectedKeysStr of string []
+        | SelectedKey of int
+        | [<CompiledName("selectedKey")>]SelectedKeyStr of string
+        | ErrorMessage of string
+        | Styles of obj
+        | Theme of ITheme
+        | Label of string
+        | Disabled of bool
+        | Value of obj
+        | Required of bool
+        interface IHTMLProp
+        static member p props =
+              props
+              |> List.fold (fun s x -> match x with
+                                       | Props x -> s @ x
+                                       | x -> (x :> IHTMLProp) :: s) []
+              |> kvl
 
-  let dropdown props = ofImport "Dropdown" ImportPath (IDropdownProps.p props)
+    let dropdown props = ofImport "Dropdown" ImportPath (IDropdownProps.p props)
 
+    // Options helpers:
+    let optionItem key text = { IDropdownOption.key = key; text = text; itemType = DropdownMenuItemType.Item }
+    let optionHeader key text = { IDropdownOption.key = key; text = text; itemType = DropdownMenuItemType.Header }
+    let optionDivider key = { IDropdownOption.key = key; text = "-"; itemType = DropdownMenuItemType.Divider }
